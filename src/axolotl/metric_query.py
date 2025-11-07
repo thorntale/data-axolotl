49a3cd2616ff6a0bb597d8af5be71a039da5a83f
John Stimac <johnstimac111@gmail.com>
2025-11-07 12:47:29 -0800

    Show a nicer in progress query display.

---
+++ src/axolotl/metric_query.py
@@ -0,0 +1,28 @@
+from typing import Callable, Dict, List, NamedTuple, Any
+from enum import Enum
+
+from .state_dao import Metric
+
+
+class QueryStatus(Enum):
+    """Status of a running query"""
+    QUEUED = "queued"
+    STARTED = "started"
+    DONE = "done"
+    ERROR = "error"
+
+class MetricQuery(NamedTuple):
+    # Query execution details
+    query: str  # SQL query string to execute
+    query_id: str  # Snowflake query ID (set after execution)
+    timeout_at: float  # monotonic time at which to timeout this query
+    status: QueryStatus  # Current status of the query
+
+    # debug info
+    fq_table_name: str
+    column_name: str
+    query_detail: str
+
+    # query results into Metric objects
+    # Takes run_id, fq_table_name, column_name, and dict of query results, returns list of Metrics
+    result_extractor: Callable[[int, str, str, Dict[str, Any]], List[Metric]]
